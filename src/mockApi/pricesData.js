5f1c14961407cc47e308af83258a9d2356d8a249
nahsiloh <lishan1512@gmail.com>
2020-02-14 11:46:54 +0800

    added mockApi data

---
+++ src/mockApi/pricesData.js
@@ -0,0 +1,100 @@
+export const USD = [
+  {
+    id: 1,
+    price: 120,
+    competitors: {
+      "Booking.com": 125,
+      "Hotels.com": 121,
+      Expedia: 120,
+      getaroom: 140,
+      "AMOMA.com": 132.77
+    }
+  },
+  { id: 2, price: 841 },
+  { id: 3, price: 715, competitors: { Expedia: 722.86, Prestigia: 590.15 } },
+  { id: 4, price: 155, competitors: { Kayak: 169.71, getaroom: 184.05 } },
+  { id: 5, price: 121, competitors: { Prestigia: 122.49, Kayak: 139.5 } },
+  {
+    id: 6,
+    price: 95,
+    competitors: { "Booking.com": 103.3, "Agoda.com": 126.67 }
+  },
+  { id: 7, price: 123 },
+  { id: 8, price: 123 }
+];
+
+export const SGD = [
+  {
+    id: 1,
+    price: 164,
+    competitors: { Traveloka: 190, Expedia: 163 },
+    taxes_and_fees: { tax: 13.12, hotel_fees: 16.4 }
+  },
+  { id: 2, price: 1150, taxes_and_fees: { tax: 92, hotel_fees: 115 } },
+  { id: 3, price: 979.74, competitors: { Expedia: 981.88, Prestigia: 801.62 } },
+  { id: 4, price: 212.39, competitors: { Kayak: 230.52, getaroom: 250 } },
+  { id: 7, price: 168.5, taxes_and_fees: { tax: 13.48, hotel_fees: 16.85 } },
+  { id: 8, price: 168.5, taxes_and_fees: { tax: 13.48, hotel_fees: 16.85 } }
+];
+
+export const CNY = [
+  {
+    id: 1,
+    price: 825.94,
+    competitors: {
+      "Booking.com": 815,
+      "Hotels.com": 817,
+      Expedia: 814,
+      getaroom: 830,
+      "AMOMA.com": 900
+    }
+  },
+  { id: 2, price: 5788.43, competitors: { JinJiang: 6000 } },
+  { id: 3, price: 4921.2, competitors: { Expedia: 4900.1, Prestigia: 4000.5 } },
+  {
+    id: 4,
+    price: 1066.83,
+    competitors: {
+      JinJiang: 900.9,
+      "Shangri-la": 990.9,
+      "Rosewood Group": 1000
+    }
+  },
+  { id: 5, price: 832.82, competitors: { Prestigia: 830, Kayak: 945.5 } },
+  { id: 6, price: 653.87, competitors: { Prestigia: 800, "Booking.com": 700 } },
+  { id: 7, price: 846.58 },
+  { id: 8, price: 846.58 }
+];
+
+export const KRW = [
+  {
+    id: 1,
+    price: 134434.8,
+    competitors: {
+      "Booking.com": 135154.89,
+      "Hotels.com": 135486.56,
+      Expedia: 134989.05,
+      getaroom: 137642.4,
+      "AMOMA.com": 149250.8
+    }
+  },
+  { id: 2, price: 942163.89 },
+  {
+    id: 3,
+    price: 801007.35,
+    competitors: { Expedia: 812604.25, Prestigia: 663419.79 }
+  },
+  { id: 4, price: 173644.95 },
+  {
+    id: 5,
+    price: 135555.09,
+    competitors: { Prestigia: 137642.4, Kayak: 156796.25 }
+  },
+  {
+    id: 6,
+    price: 106427.55,
+    competitors: { Prestigia: 132667.37, "Booking.com": 116083.95 }
+  },
+  { id: 7, price: 137795.67 },
+  { id: 8, price: 137795.67 }
+];
