365e6ab06c9a3548767edaeeae5268883df60b85
nahsiloh <lishan1512@gmail.com>
2020-02-14 11:48:19 +0800

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Hotels from "../Hotels/Hotels";
+import NavBar from "../NavBar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Hotels />
+    </div>
+  );
+}
+
+export default App;
