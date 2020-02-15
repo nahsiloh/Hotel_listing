4e33752969c8496d0b50dc65ea347c816fdade37
nahsiloh <lishan1512@gmail.com>
2020-02-15 09:29:16 +0800

    distplay hotel prices basing on currency and competitor rates

--- src/components/Hotels/Hotels.js
+++ src/components/Hotels/Hotels.js
@@ -1,43 +1,91 @@
 import React from "react";
 import HOTELS from "../../mockApi/hotelsData";
-import { USD } from "../../mockApi/pricesData";
+import { USD, SGD, CNY, KRW } from "../../mockApi/pricesData";
 import { Container, Image, Row, Col } from "react-bootstrap";
 import "./Hotels.css";
 
-const Hotels = () => {
-  const getPrice = (currency, hotelId) => {
-    const index = currency.findIndex(obj => obj.id === hotelId);
-    return currency[index].price;
+const pricesByCurrency = {
+  USD: USD,
+  SGD: SGD,
+  CNY: CNY,
+  KRW: KRW
+};
+
+class Hotels extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  isHotelPriceDataAvailable = index => {
+    if (index >= 0) {
+      return true;
+    }
   };
 
-  return (
-    <div>
-      {HOTELS.map(hotel => {
-        return (
-          <Container key={hotel.name}>
-            <Row>
-              <Col sm={4}>
-                <Image src={hotel.photo} alt={hotel.name} rounded />
-              </Col>
-              <Col sm={8}>
-                <Row>
-                  <Col sm={6}>
-                    <h6>{hotel.name}</h6>
-                    <p>{hotel.address}</p>
-                  </Col>
-                  <Col sm={6}>
-                    <p>rating: {hotel.rating}</p>
-                    <p>stars: {hotel.stars}</p>
-                    <p>price: {getPrice(USD, hotel.id)}</p>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Container>
-        );
-      })}
-    </div>
-  );
-};
+  getHotelPriceData = (currency, hotelId) => {
+    const currencySelected = pricesByCurrency[currency];
+    const index = currencySelected.findIndex(obj => obj.id === hotelId);
+    if (this.isHotelPriceDataAvailable(index)) {
+      return currencySelected[index];
+    }
+  };
+
+  getPrice = (currency, hotelId) => {
+    const hotelPriceData = this.getHotelPriceData(currency, hotelId);
+    return hotelPriceData.price;
+  };
+
+  getCompetitors = (currency, hotelId) => {
+    const hotelPriceData = this.getHotelPriceData(currency, hotelId);
+    if (hotelPriceData.competitors) {
+      return Object.entries(hotelPriceData.competitors).map(([key, value]) => (
+        <p>
+          {key} : {value}
+        </p>
+      ));
+    } else {
+      return "Best price available";
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {HOTELS.map(hotel => {
+          return (
+            <Container key={hotel.name}>
+              <Row>
+                <Col sm={4}>
+                  <Image src={hotel.photo} alt={hotel.name} rounded />
+                </Col>
+                <Col sm={8}>
+                  <Row>
+                    <Col sm={6}>
+                      <h6>{hotel.name}</h6>
+                      <p>{hotel.address}</p>
+                      <p>Compare rates:</p>
+                      <p>
+                        {this.getCompetitors(this.props.currency, hotel.id)}
+                      </p>
+                    </Col>
+                    <Col sm={6}>
+                      <p>rating: {hotel.rating}</p>
+                      <p>stars: {hotel.stars}</p>
+                      <p>
+                        price: {this.props.currency}{" "}
+                        {this.getPrice(this.props.currency, hotel.id)}
+                      </p>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default Hotels;
