39217d0fbafe2653b11cec06006ebbb12f5c35f7
nahsiloh <lishan1512@gmail.com>
2020-02-16 11:11:39 +0800

    added data-testid for selector

--- src/components/CurrencySelector/CurrencySelector.js
+++ src/components/CurrencySelector/CurrencySelector.js
@@ -14,9 +14,11 @@ class CurrencySelector extends React.Component {
   render() {
     return (
       <div>
-        <select onChange={this.changeCurrency}>
+        <select data-testid="currency_selector" onChange={this.changeCurrency}>
           {CURRENCIES.map(cur => (
-            <option value={cur}>{cur}</option>
+            <option key={cur} value={cur}>
+              {cur}
+            </option>
           ))}
         </select>
       </div>
