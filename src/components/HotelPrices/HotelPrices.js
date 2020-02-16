326f6f46eb09f9a61fc2819eeb16c024d48af76d
nahsiloh <lishan1512@gmail.com>
2020-02-16 11:11:22 +0800

    added functionality to display competitor prices

--- src/components/HotelPrices/HotelPrices.js
+++ src/components/HotelPrices/HotelPrices.js
@@ -31,22 +31,56 @@ export const getHotelPrice = (currency, hotelId) => {
   }
 };
 
+const isCheaper = (priceOnSite, competitorPrice) => {
+  if (competitorPrice > priceOnSite) {
+    return true;
+  }
+};
+
+const calculateSavings = (priceOnSite, competitorPrice) => {
+  if (isCheaper(priceOnSite, competitorPrice)) {
+    return Math.floor(
+      ((competitorPrice - priceOnSite) / competitorPrice) * 100
+    );
+  }
+};
+
+const displaySavings = (priceOnSite, competitorPrice) => {
+  const savings = calculateSavings(priceOnSite, competitorPrice);
+  if (savings > 0) {
+    return <p>savings: {savings}%</p>;
+  }
+};
+
+const isStrikethroughRate = (priceOnSite, competitorPrice) => {
+  if (isCheaper(priceOnSite, competitorPrice)) {
+    return <strike>{competitorPrice}</strike>;
+  } else {
+    return competitorPrice;
+  }
+};
+
 export const getCompetitorsPrice = (currency, hotelId) => {
   const hotelPriceData = getHotelPriceData(currency, hotelId);
+  const priceOnSite = getHotelPrice(currency, hotelId);
+
   if (hotelPriceData && hotelPriceData.competitors) {
     const priceArray = Object.entries(hotelPriceData.competitors);
-    priceArray.push(["Ascenda", getHotelPrice(currency, hotelId)]);
+    priceArray.push(["Us", priceOnSite]);
 
     const sortPrices = priceArray.sort((a, b) => {
       return [a[1]] - [b[1]];
     });
 
     return sortPrices.map(([key, value]) => (
-      <p>
-        {key} : {value}
-      </p>
+      <div key={key}>
+        <p>
+          {key} : {isStrikethroughRate(priceOnSite, value)}
+        </p>
+        {displaySavings(priceOnSite, value)}
+      </div>
     ));
   } else {
-    return "Best price available";
+    return "Best price available here";
   }
 };
