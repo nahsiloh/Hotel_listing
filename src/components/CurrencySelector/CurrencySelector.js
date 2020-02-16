68cc8d42eadc27fe7ffa9dec3f1ac798fb7b151f
nahsiloh <lishan1512@gmail.com>
2020-02-16 18:39:04 +0800

    styled currency selector

--- src/components/CurrencySelector/CurrencySelector.js
+++ src/components/CurrencySelector/CurrencySelector.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./CurrencySelector.css";
 const CURRENCIES = ["USD", "SGD", "CNY", "KRW"];
 
 class CurrencySelector extends React.Component {
@@ -14,6 +15,7 @@ class CurrencySelector extends React.Component {
   render() {
     return (
       <div>
+        <p>Select currency:</p>
         <select data-testid="currency_selector" onChange={this.changeCurrency}>
           {CURRENCIES.map(cur => (
             <option key={cur} value={cur}>
